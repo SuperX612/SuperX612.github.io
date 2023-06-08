94c1eb2cac4cf24490e1a582f04cb02641a73d0f
kunlan_xiang@163.com <kunlan_xiang@163.com>
2023-06-08 10:12:30 +0800

    Site updated: 2023-06-08 10:12:24

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,15 @@
+!function (e, t, a) {
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
